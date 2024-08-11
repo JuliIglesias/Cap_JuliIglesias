5cca80ceeeec8abc80b9e95c044fe053b0cedbec
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-08-11 13:58:46 -0300

    back cap hecho

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let db = [];
-let id = 0;
+let db = [{id: 0, name: 'Juan'}, {id: 1, name: 'Pedro'}, {id: 2, name: 'Pablo'}];
+let id = 3;
 
 app.get('/', (req, res) => {
     res.send('Hello World bitche!' +
@@ -35,11 +35,10 @@ app.post('/post', (req, res) => {
 
 app.put('/put/:id', (req, res) => {
    let id1 = parseInt(req.params.id);
-   //Number(id1);
-   //parseInt(id1,10);
-   console.log(id1);
+
    let iteme = req.body;
    console.log(iteme);
+
    let findItem = db.find(item => item.id === id1);
     console.log('Item encontrado', findItem);
 
@@ -50,6 +49,21 @@ app.put('/put/:id', (req, res) => {
 
 app.get('/get', (req, res) => {
     res.send(db);
+});
+
+app.delete('/delete/:id', (req, res) => {
+    let id1 = parseInt(req.params.id);
+
+    let findItem = db.find(item => item.id === id1);
+    console.log('Item encontrado', findItem);
+
+    if(!findItem){ res.status(404).send('No se encontro el item') }
+
+    db = db.filter(item => item.id !== id1); // se podria hacer un pop nada mas en este indice de id de la lista mas que filtrar
+
+    console.log('db', db);
+
+    res.send(findItem);
 });
 
 app.listen(port, () => {
